f68386528e5470a0a9a7ef2911cd2a5e90e6194a
Tim Irvin <irvin@nettempo.com>
2022-09-10 10:10:32 -0700

    Fixed output in entrypoint script. Version bump

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,8 @@
 
 echo -n "AWS for Fluent Bit Container Image Version "
 cat /AWS_FOR_FLUENT_BIT_VERSION
+echo
 echo -n "Watchwith for Fluent Bit Container Image Version "
 cat /WATCHWITH_CONTAINER_VERSION
+echo
 exec /fluent-bit/bin/fluent-bit -e /fluent-bit/firehose.so -e /fluent-bit/cloudwatch.so -e /fluent-bit/kinesis.so -e /fluent-bit/out_sqs.so -c /fluent-bit/etc/fluent-bit.conf
